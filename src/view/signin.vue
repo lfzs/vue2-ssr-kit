0b730b4d3a57193302bb869547d06a29f1241eaa
lfzs <jycomputer_417@163.com>
2020-10-19 14:13:57 +0800

    feat: add axios

---
+++ src/view/signin.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>登陆</h1>
+    <input v-model="mobile" />
+    <input v-model="password" />
+    <el-button @click="signin">登陆</el-button>
+  </div>
+</template>
+
+<script>
+  import { authStore } from '@/store'
+
+  export default {
+    name: 'signin',
+    data() {
+      return {
+        mobile: '',
+        password: '',
+      }
+    },
+
+    mounted() {
+      const { next } = this.$route.query
+      next && authStore.setNext(next)
+    },
+
+    methods: {
+      async signin() {
+        await authStore.signin({ mobile: this.mobile, password: this.password })
+        authStore.signinNext()
+      },
+    },
+
+  }
+</script>
