edad41f100c0fc61a19a055a2cd664cc1c862405
lfzs <jycomputer_417@163.com>
2020-11-20 14:31:29 +0800

    chore: update pkg

--- src/util/index.js
+++ src/util/index.js
@@ -7,5 +7,6 @@ export function sleep(time = 0) {
 
 // 从 response 中获取 message
 export function getErrorMessage(response, defaultMessage = '请求失败, 请重试') {
+  if (typeof response === 'string') return response
   return response?.message ?? defaultMessage
 }
