1d6a701cd556805299abf997c124ca36cdd7100c
lfzs <jycomputer_417@163.com>
2020-11-20 14:06:58 +0800

    feat: use response data

--- src/store/helper/Cache.js
+++ src/store/helper/Cache.js
@@ -8,12 +8,12 @@ export default class {
   static findOrCreate(id = isRequired()) {
     this._id = this._id || Math.random().toString(36).slice(2) // 向构造函数添加 _id 标志
 
-    const key = `${this._id}${id}`
+    const key = `${this._id}_${id}`
     let value = this.caches[key]
 
     if (!value) {
       value = new this(id)
-      value.id = id // 将 id 挂载到对应实例
+      value.id = id
       this.caches[key] = value
     }
 
@@ -22,7 +22,7 @@ export default class {
 
   static remove(id) {
     if (this._id && id) {
-      const key = `${this._id}${id}`
+      const key = `${this._id}_${id}`
       const value = this.caches[key]
       value && delete this.caches[key]
     }
